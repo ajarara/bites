30c12318834bfa6085657f406e093f8afc385e8e
Ahmad Jarara <ajarara@fastmail.com>
2025-02-15 17:24:50 -0500

    Add more info to fat_jar

--- toolchains/internal/fat_jar.sh
+++ toolchains/internal/fat_jar.sh
@@ -1,2 +1,6 @@
+# --jar_builder_tool --zip_scrubber_tool --output buck-out/v2/gen/root/904931f735703749/__MyGreatApp__/MyGreatApp.jar --jars_file buck-out/v2/gen/root/904931f735703749/__MyGreatApp__/jars_file --main_class io.ajarara.example.Example
+echo "fat_jar"
+pwd
+env
 echo "fat_jar"
 echo $@
