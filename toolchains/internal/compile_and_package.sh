150dc37cecbed92cced8cebb38918006987c2106
Ahmad Jarara <ajarara@fastmail.com>
2025-02-16 16:06:05 -0500

    More cleanup

--- toolchains/internal/compile_and_package.sh
+++ toolchains/internal/compile_and_package.sh
@@ -3,17 +3,11 @@ set -o nounset
 set -o pipefail
 set -eu
 
-# --jar_builder_tool --output buck-out/v2/gen/root/904931f735703749/java/io/ajarara/example/__example__/jar/example.jar --javac_tool javac --generated_sources_dir buck-out/v2/gen/root/904931f735703749/java/io/ajarara/example/__example__/generated_sources --javac_args_file buck-out/v2/gen/root/904931f735703749/java/io/ajarara/example/__example__/javac_args
-
 while [[ $# -gt 0 ]]; do
     case $1 in
         --jar_builder_tool)
-            # ignore it, unsure why it's passed
-            shift 1
-            ;;
-        '')
-            # ignore this too, unsure why it's passed
-            shift 1
+            JAR_BUILDER_TOOL="$2"
+            shift 2
             ;;
         --output)
             OUTPUT="$2"
@@ -42,15 +36,14 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# solo dependencies don't have anything on the path
+[[ -n JAR_BUILDER_TOOL ]] && JAR_BUILDER_TOOL="jar"
+
+# solo dependencies don't have anything on the path, so the argument might be omitted altogether
 [[ -v JAVAC_CLASSPATH_FILE ]] || JAVAC_CLASSPATH_FILE=""
 
 $JAVAC_TOOL @$JAVAC_ARGS_FILE -d $TMPDIR -cp @$JAVAC_CLASSPATH_FILE
 
-JAR_NAME="$(basename $OUTPUT)"
-
-jar --create --file $JAR_NAME -C $TMPDIR/ .
-mv $JAR_NAME $OUTPUT
+$JAR_BUILDER_TOOL --create --file $OUTPUT -C $TMPDIR/ .
 
 # generated sources? ignore for now.
 mkdir -p $GENERATED_SOURCES_DIR
