36551f8d1f93d6d6ba77aa3982d7e72397d6d6ff
Ahmad Jarara <ajarara@fastmail.com>
2025-03-04 17:13:12 -0500

    Get test case runner working for tarjan

--- java/io/ajarara/tarjan/Tarjan.java
+++ java/io/ajarara/tarjan/Tarjan.java
@@ -4,28 +4,41 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 class Tarjan {
 
     public static void main(String[] args) {
-        final Map<Integer, List<Integer>> adjA = new HashMap<>();
+        final Map<Integer, List<Integer>> adjA = new HashMap<Integer, List<Integer>>();
+        adjA.put(1, Arrays.asList(2));
         
-        testCase(tarjan(adjA), new ArrayList<Set<Integer>>());
+        testCase("adjA", tarjan(adjA), Arrays.asList(new HashSet<>()));
         
     }
 
-    private static <T> void testCase(
+    private static <T> void testCase(String name,
                                      List<Set<T>> expected,
                                      List<Set<T>> actual) {
         if (!expected.equals(actual)) {
-            System.out.println(""
+            System.out.println("Test case " + name + " failed!\nExpected: " + expected + "\nActual: " + actual);
         }
     }
 
     // returns a topo sort of strongly connected components given an adjacency list
-    private static List<Set<Integer>> tarjan() {
+    private static List<Set<Integer>> tarjan(Map<Integer, List<Integer>> adjacencyMap) {
         return new ArrayList<>();
+    }
+
+    // returns an integer index that should be used for recursive calls, otherwise mutates the lowlink, index mappings, and the stack
+    private static int strongConnect(Integer vertex, 
+                                     Map<Integer, List<Integer>> adjacencyMap,
+                                     Map<Integer, Integer> indices,
+                                     Map<Integer, Integer> lowlinks,
+                                     LinkedHashSet<Integer> stack) {
+        return 0;
+
     }
 }
