aaf5744920274ec5208e1b6305dfad84bb4da419
Ahmad Jarara <ajarara@fastmail.com>
2025-04-14 16:34:40 -0400

    Fix typescript generation
    Works so far

---
+++ typescript/src/example.ts
@@ -0,0 +1,17 @@
+const world = 'world';
+
+export function hello(who: string = world): string {
+  return `Hello ${who}! `;
+}
+
+// in place sort
+export function heapify(in: number[]): number[] {
+  
+
+}
+
+function siftDown(arr: number[], i: number, bound: number) {
+  let largest = i;
+  let left = 2 * i + 1;
+
+}
