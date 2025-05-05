9f60b35055cdc288160ba8fd0da5e3ff177ac6e2
Ahmad Jarara <ajarara@fastmail.com>
2025-05-04 20:09:28 -0400

    Add execute attempt

---
+++ jvm/src/main/java/io/ajarara/tasks/Tasks.kt
@@ -0,0 +1,8 @@
+package io.ajarara.tasks
+
+
+fun execute(tasks: List<String>, cooldown: Int) {
+    // val tasks.groupBy { it }
+    //     .mapValues { it.length }
+    //     .toMutableMap() 
+}
---
+++ jvm/src/test/java/io/ajarara/tasks/TasksTest.kt
@@ -0,0 +1,23 @@
+package io.ajarara.tasks
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+
+class TasksTest {
+    @Test
+    fun `case 1`() {
+        // ["X","X","Y","Y"], 2 out 5
+        assertEquals(
+            5,
+            execute(listOf("x", "x", "y", "y"), 2))
+    }
+
+    @Test
+    fun `case 2`() {
+        // tasks = ["A","A","A","B","C"], n = 3 out 9
+        assertEquals(
+            9,
+            execute(listOf("a", "a", "a", "b", "c"), 3))
+    }
+}
