69b1e1b1119d407858e8d84e39fce61b15252226
Ahmad Jarara <ajarara@fastmail.com>
2025-05-07 15:25:46 -0400

    Add mvnd

--- manifest.scm
+++ manifest.scm
@@ -3,6 +3,7 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages compression)
   #:use-module (guix build-system copy)
+  #:use-module (nonguix build-system binary)
   #:use-module (guix download)
   #:use-module (guix gexp)
   #:use-module (guix packages)
@@ -35,10 +36,40 @@
     (description "")
     (home-page "")
     (license license:expat)))
+
+;; this needs us to package graalvm, so we're going to use the binary for now
+(define mvnd
+  (package
+    (name "mvnd")
+    (version "1.0.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://downloads.apache.org/maven/mvnd/"
+             version
+             "/maven-mvnd-"
+             version
+             "-linux-amd64.zip"))
+       (sha256
+        (base32
+         "0krc49jfz2yqawzinncplxgrcd75905xp2yy72pya27mcm2yqd35"))))
+    (build-system binary-build-system)
+    (arguments
+     ;; set interpreter, everything else is static
+     `(#:patchelf-plan '(("bin/mvnd"))))
+    (native-inputs
+     (list unzip))
+    (synopsis "")
+    (description "")
+    (home-page "")
+    ;; gpl but for now who cares
+    (license license:expat)))
+     
  
 (concatenate-manifests
  (list
   (packages->manifest
-   (list kotlin))
+   (list kotlin mvnd))
   (specifications->manifest
    (list "openjdk:jdk" "xdot" "maven" "inotify-tools"))))
