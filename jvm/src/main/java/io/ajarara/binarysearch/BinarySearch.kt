e4a70d0bfead4ffb606f4cfeda748cc2214d0f49
Ahmad Jarara <ajarara@fastmail.com>
2025-05-13 18:01:31 -0400

    Write simple binary search, fix rotated

--- jvm/src/main/java/io/ajarara/binarysearch/BinarySearch.kt
+++ jvm/src/main/java/io/ajarara/binarysearch/BinarySearch.kt
@@ -1,23 +1,49 @@
 package io.ajarara.binarysearch
 
+fun simple(ls: List<Int>, target: Int): Int {
+    if (ls.isEmpty()) {
+        return -1
+    }
+    var l = 0
+    var r = ls.size - 1
+    while (l <= r) {
+        val mid = (l + r) / 2
+        val guess = ls[mid]
+
+        if (guess == target) {
+            return mid
+        } else if (guess < target) {
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+    return -1
+}
+
+// returns the minimum in the rotated sorted array
 fun rotated(ls: List<Int>): Int {
+    check(ls.isNotEmpty()) {
+        "impossible to return an answer"
+    }
     var l = 0
     var r = ls.size - 1
     while (l < r) {
-        val pivot: Int = (r - l) / 2
+        val mid: Int = (l + r) / 2
+        println("$l, $mid, $r")
+        
         if (ls[l] < ls[r]) {
-            // we found the minimum, it's l
-            r = l
-        } else if (ls[pivot] < ls[l]) {
-            // min is between pivot and left
-            r = pivot
-        } else if (ls[pivot] < ls[r]) {
-            // ditto
-            r = pivot
+            return ls[l]
+        } else if (ls[l] < ls[mid] || ls[mid] > ls[r]) {
+            // then the pivot must be between mid and r
+            if (mid == l) {
+                l++
+            } else {
+                l = mid + 1
+            }
         } else {
-            l = pivot
+            r = mid
         }
-        println("$l, $r, $pivot")
     }
     return ls[l]
 }
--- jvm/src/test/java/io/ajarara/binarysearch/BinarySearchTest.kt
+++ jvm/src/test/java/io/ajarara/binarysearch/BinarySearchTest.kt
@@ -5,27 +5,53 @@ import org.junit.jupiter.api.Assertions.*
 
 class BinarySearchTest {
 
-    // @Test
+    @Test
     fun ex1 () {
         assertEquals(
             1,
+            //             0  1  2  3  4  5
             rotated(listOf(3, 4, 5, 6, 1, 2))
         )
     }
 
-    // @Test
+    @Test
     fun ex2 () {
         assertEquals(
             0,
+            //             0  1  2  3  4  5
             rotated(listOf(4, 5, 0, 1, 2, 3))
         )
     }
 
-    // @Test
+    @Test
     fun ex3 () {
         assertEquals(
             4,
             rotated(listOf(4, 5, 6, 7))
+        )
+    }
+
+    @Test
+    fun ex4 () {
+        assertEquals(
+            1,
+            rotated(listOf(1))
+        )
+    }
+
+    @Test
+    fun ex5 () {
+        assertEquals(
+            1,
+            rotated(listOf(1, 2, 3))
+        )
+    }
+
+    @Test
+    fun simple1 () {
+        assertEquals(
+            3,
+            simple(listOf(1, 2, 3, 4), 4)
         )
     }
 
