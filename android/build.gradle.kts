9e45b2bf1bd69c2d46d91a1cc0464f2a6de644f4
Ahmad Jarara <ajarara@fastmail.com>
2025-08-08 10:39:53 -0400

    Update to kotlin script for rootproject

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.agpVersion = "8.5.0"
+
+    val agpVersion = "8.5.0"
     repositories {
         // Insert local test repo here
         google()
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:$agpVersion"
+        classpath("com.android.tools.build:gradle:$agpVersion")
     }
 }
 
@@ -20,14 +21,12 @@ allprojects {
     }
 }
 
-ext {
-    buildToolsVersion = "32.0.0"
-    androidxAnnotationVersion = "1.5.0"
-    guavaVersion = "31.1-android"
-    coreVersion = "1.6.1"
-    extJUnitVersion = "1.2.1"
-    runnerVersion = "1.6.1"
-    rulesVersion = "1.6.1"
-    espressoVersion = "3.6.1"
-    uiAutomatorVersion = "2.3.0"
-}
+extra["buildToolsVersion"] = "32.0.0"
+extra["androidxAnnotationVersion"] = "1.5.0"
+extra["guavaVersion"] = "31.1-android"
+extra["coreVersion"] = "1.6.1"
+extra["extJUnitVersion"] = "1.2.1"
+extra["runnerVersion"] = "1.6.1"
+extra["rulesVersion"] = "1.6.1"
+extra["espressoVersion"] = "3.6.1"
+extra["uiAutomatorVersion"] = "2.3.0"
