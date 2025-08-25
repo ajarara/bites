2c7aeb9cf70db503004296035b12ca25e76422ef
Ahmad Jarara <ajarara@fastmail.com>
2025-08-25 15:22:48 -0400

    Move over to ViewBinding for MainActivity

--- android/app/src/main/java/com/example/android/testing/uiautomator/BasicSample/MainActivity.kt
+++ android/app/src/main/java/com/example/android/testing/uiautomator/BasicSample/MainActivity.kt
@@ -2,31 +2,24 @@ package com.example.android.testing.uiautomator.BasicSample
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import com.example.android.testing.uiautomator.BasicSample.databinding.ActivityMainBinding
 
 class MainActivity: Activity() {
-    private lateinit var textView: TextView
-    private lateinit var editText: EditText
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super<Activity>.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
 
-
-        findViewById<View>(R.id.changeTextBt).setOnClickListener {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.changeTextBt.setOnClickListener {
             // First button's interaction: set a text in a text view.
-            textView.text = editText.text.toString()
+            binding.textToBeChanged.text = binding.editTextUserInput.text.toString()
         }
-        findViewById<View>(R.id.activityChangeTextBtn).setOnClickListener {
+        binding.activityChangeTextBtn.setOnClickListener {
             // Second button's interaction: start an activity and send a message to it.
-            val intent = ShowTextActivity.newStartIntent(this, editText.text.toString())
+            val intent = ShowTextActivity.newStartIntent(this, binding.editTextUserInput.text.toString())
             startActivity(intent)
         }
-        textView = findViewById(R.id.textToBeChanged)
-        editText = findViewById(R.id.editTextUserInput)
 
     }
 }
