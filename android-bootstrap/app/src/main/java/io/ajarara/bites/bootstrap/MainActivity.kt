f65a8bbd2e453f6c415bd43900ac4fbe9fccdba1
Ahmad Jarara <ajarara@fastmail.com>
2025-09-06 23:46:05 -0400

    Remove more shreds of compose from auto generated project

--- android-bootstrap/app/src/main/java/io/ajarara/bites/bootstrap/MainActivity.kt
+++ android-bootstrap/app/src/main/java/io/ajarara/bites/bootstrap/MainActivity.kt
@@ -3,28 +3,8 @@ package io.ajarara.bites.bootstrap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-import io.ajarara.bites.bootstrap.ui.theme.BootstrapTheme
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BootstrapTheme {
-        Greeting("Android")
     }
 }
