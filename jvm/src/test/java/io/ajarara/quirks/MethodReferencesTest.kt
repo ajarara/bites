794c9960c76dd9fc907aac9c56547e249e2ac8c0
Ahmad Jarara <ajarara@fastmail.com>
2025-11-10 19:07:16 -0500

    Attempt to ferret out leetcode bug

---
+++ jvm/src/test/java/io/ajarara/quirks/MethodReferencesTest.kt
@@ -0,0 +1,17 @@
+package io.ajarara.quirks
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+class MethodReferencesTest {
+
+    @Test
+    fun `simple case`() {
+        val map = mutableMapOf<Int, MutableSet<Int>>()
+
+        val set1 = map.computeIfAbsent(1, ::mutableSetOf)
+        val set2 = map.computeIfAbsent(2, ::mutableSetOf)
+
+        check(set1 === set2)
+    }
+}
